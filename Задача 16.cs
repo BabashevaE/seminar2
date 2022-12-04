7f9e9862bb10fe5e6b7d597d5ed2ae24cc56a706
BabashevaE <118519551+BabashevaE@users.noreply.github.com>
2022-12-04 16:42:28 +0300

    Add files via upload

---
+++ Задача 16.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите первое число");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+if (Num1 * Num1 == Num2) 
+    Console.WriteLine("является квадратом");
+else 
+{
+    if (Num2 * Num2 == Num1) 
+    Console.WriteLine("является квадратом");
+    else 
+    Console.WriteLine("не является квадратом");
+}
